72a92441c78d495031ea5b8ce6162d4031914f84
melaniemdm <melanieborja.pro@gmail.com>
2024-10-18 17:53:26 +0200

    test : current user

---
+++ src/test/java/com/chatop/api/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.chatop.api.controller;
+
+import com.chatop.api.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
+    @Test
+    public void testGetUsers() throws Exception{
+        // Mock du service pour renvoyer une liste vide d'utilisateurs
+        Mockito.when(userService.getUsers()).thenReturn(Collections.emptyList());
+
+        // Test de la requête GET /api/users (à cause de @RequestMapping("/api"))
+        mockMvc.perform(get("/auth/me"))
+                .andExpect(status().isOk());// Vérifie que le statut HTTP est 200 OK
+    }
+}
